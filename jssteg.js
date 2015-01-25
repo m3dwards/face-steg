35a5a8c15866ef46b718a3202af58b56fbc1e87e
maxwe789 <youwontforgetthis@gmail.com>
2015-01-25 11:37:03 +0000

    Update jssteg.js

--- jssteg.js
+++ jssteg.js
@@ -99,21 +99,21 @@ var objectURL;
 
 // Bind the decode button to reading out the coefficients from the image. Note the image must be a
 // JPEG for this to do something sensible.
-document.getElementById("decodeButton").addEventListener("click", function() {
+document.getElementById("decode").addEventListener("click", function() {
   jsSteg.getCoefficients(objectURL, readCoefficients);
 });
 
 // Bind the encode button to reading out the coefficients from the image
-document.getElementById("encodeButton").addEventListener("click", function () {
+document.getElementById("encode").addEventListener("click", function () {
   jsSteg.reEncodeWithModifications(objectURL, modifyCoefficients, function (resultUri) {
     // Do whatever you want with the resulting uri, in this example we display it on the page.
-    document.getElementById("encodedImage").src = resultUri;
+    document.getElementById("output").src = resultUri;
   });
 });
 
 // When the user completes selecting an image we generate a blob url for it to communicate with 
 // the encoder/decoder
-document.getElementById("image-select").addEventListener("change", function handleFileSelect(e){
+document.getElementById("file").addEventListener("change", function handleFileSelect(e){
   // Ensure the user chose a single file
   if (e.target.files.length !== 1) {
     throw new Error("User didn't select a file correctly");
@@ -125,5 +125,5 @@ document.getElementById("image-select").addEventListener("change", function hand
   // Clear the file selection
   e.target.files = [];
   // Reset the 'encoded image' being displayed
-  document.getElementById("encodedImage").src = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+  document.getElementById("output").src = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
 }, false);
